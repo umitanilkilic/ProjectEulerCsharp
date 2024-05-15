8bf4452f5e5abfa22e3e7ad82dfd5b19cc25e420
umit <umitanilkilic@gmail.com>
2024-05-15 16:38:23 +0300

    The complexity of the testsolutions() function was reduced.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 using System.Reflection;
 using Newtonsoft.Json;
+
 static class Program
 {
     static void Main(string[] args)
@@ -12,7 +13,11 @@ static class Program
 
     static void TestSolutions()
     {
-        Type[] classesInSolutionsNamespace = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.Namespace == "Solutions" && x.IsClass).ToArray();
+        Type[] classesInSolutionsNamespace = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(x => x.Namespace == "Solutions" && x.IsClass)
+            .ToArray();
 
         foreach (Type item in classesInSolutionsNamespace)
         {
@@ -35,24 +40,20 @@ static class Program
                     var answer = (string)(answerField?.GetValue(instance) ?? "");
                     var trueAnsw = GetAnswersFromJson(problemId);
 
-                    Console.Write("Problem ID: {0}, ", problemId);
-                    if (!CheckAnswer(answer, trueAnsw))
-                    {
-                        Console.WriteLine("Want: {0} Got: {1}", trueAnsw, answer);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Passed");
-                    }
+                    var isCorrect = CheckAnswer(answer, trueAnsw);
 
+                    Console.WriteLine(
+                        "Problem ID: {0}, Result: {1}",
+                        problemId,
+                        isCorrect ? "Passed" : $"Want: {trueAnsw} Got: {answer}"
+                    );
                 }
             }
         }
     }
 
-
-
     static AnswerContainer? answers;
+
     static void ReadJsonFile()
     {
         string jsonFilePath = "answers.json";
@@ -62,17 +63,13 @@ static class Program
         answers = JsonConvert.DeserializeObject<AnswerContainer>(json);
     }
 
-
-
     static string GetAnswersFromJson(int id)
     {
         return answers?.questions?.FirstOrDefault(x => x.id == id)?.answer ?? "";
     }
 
-
     static bool CheckAnswer(string? a, string? b)
     {
         return a != null && b != null && a.Equals(b);
     }
-
 }
