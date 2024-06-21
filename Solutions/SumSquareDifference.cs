d83eb967ef56f16522dd6db7e43edf32162b8288
Anil <umitanilkilic@gmail.com>
2024-06-21 03:12:08 +0300

    problem_6 solution added

---
+++ Solutions/SumSquareDifference.cs
@@ -0,0 +1,22 @@
+namespace Solutions;
+
+public class SumSquareDifference : ProblemSolverBase
+{
+    /// <summary>
+    /// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    /// </summary>
+    public SumSquareDifference()
+    {
+        ProblemID = 6;
+    }
+    public override void Solve()
+    {
+        //If you don't count zero as a natural number, then the correct answer is 25164150.
+        int[] range = new int[2]{0,100};  
+        var totalSquaredSum = Enumerable.Range(range[0], range[1]).Select(x => x * x).Sum();
+        var sumOfSquares = Enumerable.Range(range[0], range[1]).Sum();
+        sumOfSquares *= sumOfSquares;
+
+        Answer = (sumOfSquares - totalSquaredSum).ToString();
+    }
+}
